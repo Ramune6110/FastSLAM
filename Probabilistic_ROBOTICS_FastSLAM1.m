af4a5737f02690f36bc6a68a2cb5eddf3e68639f
Ramune6110 <Arduino6110@gmail.com>
2020-05-16 01:04:23 +0900

    [Fix]Perfect

--- Probabilistic_ROBOTICS_FastSLAM1.m
+++ Probabilistic_ROBOTICS_FastSLAM1.m
@@ -49,7 +49,8 @@ flag = zeros(size(LM, 1), NP);
 mu = zeros(2 * NP, size(LM, 1));
 % 各パーティクルが持つランドマーク共分散行列
 Sigma = 100000 * ones(2 * NP, 2 * size(LM, 1));
-
+% Animation
+AnimationFlag = true;
 tic;
 % Main loop
 for i = 1 : nSteps
@@ -104,32 +105,66 @@ for i = 1 : nSteps
     result.xEst  = [result.xEst;xEst'];
     
     %Animation (remove some flames)
-    if rem(i,5)==0 
-        hold off;
-        arrow=0.5;
-        %パーティクル表示
-        for ip=1:NP
-            quiver(px(1,ip),px(2,ip),arrow*cos(px(3,ip)),arrow*sin(px(3,ip)),'ok');hold on;
-        end
-        plot(result.xTrue(:,1),result.xTrue(:,2),'k');hold on;
-        plot(LM(:,1), LM(:,2),'pk','MarkerSize',10);hold on;
-        %観測線の表示
-        if~isempty(z)
-            for iz=1:length(z(:, 1))
-                ray=[xTrue(1:2)'; z(iz,3:4)];
-                plot(ray(:,1), ray(:,2),'-g');hold on;
-            end
-        end
-        plot(result.xEst(:,1),result.xEst(:,2),'.r');hold on;
-        axis equal;
-        grid on;
-        drawnow;
+    if AnimationFlag == true
+        Animation(i, NP, px, xTrue, result, LM, z, mu, Sigma);
     end
 end
 toc
 
 DrawGraph(result, LM, mu);
 
+function [] = Animation(i, NP, px, xTrue, result, LM, z, mu, Sigma)
+    if rem(i,5)==0 
+    hold off;
+    arrow=0.5;
+    %パーティクル表示
+    for ip=1:NP
+        quiver(px(1,ip),px(2,ip),arrow*cos(px(3,ip)),arrow*sin(px(3,ip)),'ok');hold on;
+    end
+    plot(result.xTrue(:,1),result.xTrue(:,2),'k');hold on;
+    plot(LM(:,1), LM(:,2),'pk','MarkerSize',10);hold on;
+    %観測線の表示
+    if~isempty(z)
+        for iz=1:length(z(:, 1))
+            ray=[xTrue(1:2)'; z(iz,3:4)];
+            plot(ray(:,1), ray(:,2),'-g');hold on;
+        end
+    end
+    ShowErrorEllipse(z, mu, Sigma);
+    plot(result.xEst(:,1),result.xEst(:,2),'.r');hold on;
+    axis equal;
+    grid on;
+    drawnow;
+    end
+end
+
+function [] = ShowErrorEllipse(z, mu, Sigma)
+    % caluclate eig, eig_valus
+    for i = 1:length(z(:, 1))
+        [eig_vec, eig_valus] = eig(Sigma(298 : 299, i * 3 - 2: i * 3 - 1));
+        % eig comparizon
+        if eig_valus(1, 1) >= eig_valus(2, 2)
+            long_axis  = 3 * sqrt(eig_valus(1, 1));
+            short_axis = 3 * sqrt(eig_valus(2, 2));
+            angle      = atan2(eig_vec(1, 2), eig_vec(1, 1));
+        else
+            long_axis  = 3 * sqrt(eig_valus(2, 2));
+            short_axis = 3 * sqrt(eig_valus(1, 1));
+            angle      = atan2(eig_vec(2, 2), eig_vec(2, 1));
+        end
+        % make Ellipse
+        t = 0:10:360;
+        x = [long_axis * cosd(t); short_axis * sind(t)];
+        if(angle < 0)
+            angle = angle + 2*pi;
+        end
+        % Ellipse Rotation
+        Rr = [cos(angle) sin(angle); -sin(angle) cos(angle)];
+        x = Rr * x;
+        plot(x(1, :) + mu(298, i), x(2, :) + mu(299, i), '-.b', 'linewidth', 1.0); hold on;
+        plot(mu(298, i), mu(299, i), 'pentagram', 'MarkerSize', 15); hold on;
+    end
+end
 % function [px_res, pw_res] = Resampling( px, pw, Nth, NP)
 %     Neff   = 1.0 / (pw * pw');
 %     if Neff < Nth
@@ -161,13 +196,13 @@ function [px_res, pw_res] = Resampling(px, pw, NP)
     pw_res = ones(1, NP) / NP;
 end
 
-function pw=Normalize(pw,NP)
+function pw = Normalize(pw,NP)
     %重みベクトルを正規化する関数
-    sumw=sum(pw);
-    if sumw~=0
-        pw=pw/sum(pw);%正規化
+    sumw = sum(pw);
+    if sumw ~= 0
+        pw = pw / sum(pw);%正規化
     else
-        pw=zeros(1,NP)+1/NP;
+        pw = zeros(1,NP) + 1/NP;
     end
 end
 
@@ -193,11 +228,9 @@ function x = f(x, u)
 end
 
 function u = doControl()
-    %Calc Input Parameter
     % [V yawrate]
-    V = 0.2; % [m/s]
+    V = 0.2;        % [m/s]
     yawrate = 10.0; % [deg/s]
-
     u =[V; toRadian(yawrate)]';
 end
 
@@ -254,12 +287,13 @@ function []=DrawGraph(result, LM, mu)
     hold off;
     x=[result.xTrue(:,1:2) result.xEst(:,1:2)];
     set(gca, 'fontsize', 16, 'fontname', 'times');
-    plot(x(:,1), x(:,2),'-.b','linewidth', 4); hold on;
+    plot(x(:,1), x(:,2),'k','linewidth', 4); hold on;
     plot(x(:,3), x(:,4),'r','linewidth', 4); hold on;
     plot(LM(:, 1), LM(:, 2), 'pentagram', 'MarkerSize', 15, 'MarkerFaceColor', 'g', 'MarkerEdgeColor', 'blue'); hold on;
     for i = 1 : size(LM, 1)
         plot(mu(298, i), mu(299, i), 'pentagram', 'MarkerSize', 15); hold on;
     end
+    xlim([-5 5]); ylim([-5 5]);
     title('PF Localization Result', 'fontsize', 16, 'fontname', 'times');
     xlabel('X (m)', 'fontsize', 16, 'fontname', 'times');
     ylabel('Y (m)', 'fontsize', 16, 'fontname', 'times');
