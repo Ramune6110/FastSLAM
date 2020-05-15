ff0d56cc208b193b9d3d26e6a63c4812ceb4b94b
Ramune6110 <Arduino6110@gmail.com>
2020-05-15 23:33:29 +0900

    [Fix]Landmark

--- FastSLAM_MK.m
+++ FastSLAM_MK.m
@@ -34,15 +34,33 @@ global Rsigma
 Rsigma = diag([0.1 toRadian(1)]).^2;
 
 %LMタグの位置 [x, y]
-LM = [10 0;
-      10 10;
-      0 15;
-     -5 20];
+% LM = [10 0;
+%       10 10;
+%       0 15;
+%       10 25;
+%       0 25;
+%      -5 20];
+LM = [10 5;
+      3 9;
+      2 5;
+      8 15;
+      10 25;
+      5 25;
+      0 30;
+      -10 25;
+      -5 15;
+      0 20;
+      -5 25;
+      -15 10];
 
-MAX_RANGE = 20;%最大観測距離
+MAX_RANGE = 30;%最大観測距離
 NP        = 100;%パーティクル数
 NTh       = NP / 2.0;%リサンプリングを実施する有効パーティクル数
 
+% % ランドマーク位置と対応変数
+% LM = zeros(30, 2);
+% LM(:, 1:2) = -20 + (20 + 20) * rand(30, 2);
+ 
 px = repmat(xEst, 1, NP);%パーティクル格納変数
 pw = zeros(1, NP) + 1 / NP;%重み変数
 % 観測値に対するflag
@@ -50,7 +68,7 @@ flag = zeros(size(LM, 1), NP);
 % 各パーティクルが持つランドマーク位置
 mu = zeros(2 * NP, size(LM, 1));
 % 各パーティクルが持つランドマーク共分散行列
-Sigma = zeros(2 * NP, 2 * size(LM, 1));
+Sigma = 100000 * ones(2 * NP, 2 * size(LM, 1));
 
 tic;
 % Main loop
@@ -67,28 +85,28 @@ for i = 1 : nSteps
         % Dead Reckoning and random sampling
         x = f(x, u) + sqrt(Q) * randn(3,1);
         % Calc Inportance Weight
-        for iz=1:length(z(:, 1))
+        for iz = 1:length(z(:, 1))
             if (flag(iz, ip) < 1.0)
                 % LMを初めて観測する場合
                 % 観測値の平均値の初期化
-                mu(3 * ip - 2 : 3 * ip - 1, iz) = xEst(1 : 2) + [z(iz, 1) * cos(xEst(3) + z(iz, 2)); z(iz, 1) * sin(xEst(3) + z(iz, 2))]; % (8.44)式
+                mu(3 * ip - 2:3 * ip - 1, iz) = xEst(1 : 2) + [z(iz, 1) * cos(PI2PI(xEst(3) + z(iz, 2))); z(iz, 1) * sin(PI2PI(xEst(3) + z(iz, 2)))]; % (8.44)式
                 % ヤコビアンH行列
-                H = jacobian_H(z, x);
+                H = jacobian_H(mu, x);
                 % 観測値の共分散行列初期化
-                Sigma(3 * ip - 2 : 3 * ip - 1, 3 * iz - 2: 3 * iz - 1) = inv(H' / R * H); %(8,48)式
+                Sigma(3 * ip - 2:3 * ip - 1, 3 * iz - 2:3 * iz - 1) = inv(H' / R * H); %(8,48)式
                 flag(iz, ip)     = 1.0;
             else
                 % LMの観測が初めてではない場合
-                [expectedZ, H] = measurement_model(mu(3 * ip - 2 : 3 * ip - 1, iz), x);
+                [expectedZ, H] = measurement_model(mu(3 * ip - 2:3 * ip - 1, iz), x);
                 % 計測の共分散
-                Rt = (H * Sigma(3 * ip - 2 : 3 * ip - 1, 3 * iz - 2: 3 * iz - 1) * H') + R; % (8.38)式の括弧の中
+                Rt = (H * Sigma(3 * ip - 2:3 * ip - 1, 3 * iz - 2:3 * iz - 1) * H') + R; % (8.38)式の括弧の中
                 % カルマンゲインの計算
-                K = (Sigma(3 * ip - 2 : 3 * ip - 1, 3 * iz - 2: 3 * iz - 1) * H') / Rt; % (8.38)式
+                K = (Sigma(3 * ip - 2:3 * ip - 1, 3 * iz - 2:3 * iz - 1) * H') / Rt; % (8.38)式
                 % 平均値の更新
                 error         = [z(iz, 1) - expectedZ(1); z(iz, 2) - expectedZ(2)];
-                mu(3 * ip - 2 : 3 * ip - 1, iz) = mu(3 * ip - 2 : 3 * ip - 1, iz) + K * error; %(8.37)式
+                mu(3 * ip - 2:3 * ip - 1, iz) = mu(3 * ip - 2:3 * ip - 1, iz) + K * error; %(8.37)式
                 % 共分散の更新
-                Sigma(3 * ip - 2 : 3 * ip - 1, 3 * iz - 2: 3 * iz - 1) = (eye(2) - K * H) * Sigma(3 * ip - 2 : 3 * ip - 1, 3 * iz - 2: 3 * iz - 1); % (8.40)式
+                Sigma(3 * ip - 2:3 * ip - 1, 3 * iz - 2: 3 * iz - 1) = (eye(2) - K * H) * Sigma(3 * ip - 2:3 * ip - 1, 3 * iz - 2:3 * iz - 1); % (8.40)式
                 w                = w * likelihood(error, Rt);
             end
         end
@@ -183,14 +201,28 @@ function u = doControl(time)
     % [V yawrate]
     V=1.0; % [m/s]
     yawrate = 5; % [deg/s]
+%     
+%     if time > 10 && time <= 15
+%         u = [V; pi/10.0];
+%     elseif time > 15 && time <= 30
+%         u = [V; 0.0];
+%     elseif time > 30 && time <= 35
+%         u = [V; pi/10.0];
+%     elseif time > 35 && time <= 50
+%         u = [V; 0.0];
+%     elseif time > 50 &&time <= 55
+%         u = [V; pi/10.0];
+%     else
+%         u = [V; 0.0];
+%     end     
     
     u =[ V*(1-exp(-time/T)) toRadian(yawrate)*(1-exp(-time/T))]';
 end
 
 function H = jacobian_H(z, x)
     % ヤコビアン行列H
-    H = [(z(3) - x(1)) / z(1) (z(4) - x(2)) / z(1)^2;
-         (x(2) - z(4)) / z(1) (z(3) - x(1)) / z(1)^2];
+    H = [(z(3) - x(1)) / z(1) (z(4) - x(2)) / z(1);
+         (x(2) - z(4)) / z(1)^2 (z(3) - x(1)) / z(1)^2];
 end
 
 function [expectedZ, H] = measurement_model(mu, x)
@@ -199,8 +231,8 @@ function [expectedZ, H] = measurement_model(mu, x)
     distance  = sqrt(dx^2 + dy^2);
     theta     = atan2(dy, dx) - x(3);
     expectedZ = [distance; PI2PI(theta)];
-    H         = [(mu(1) - x(1)) / expectedZ(1) (mu(2) - x(2)) / expectedZ(1)^2;
-                 (x(2) - mu(2)) / expectedZ(1) (mu(1) - x(1)) / expectedZ(1)^2];
+    H         = [(mu(1) - x(1)) / expectedZ(1) (mu(2) - x(2)) / expectedZ(1);
+                 (x(2) - mu(2)) / expectedZ(1)^2 (mu(1) - x(1)) / expectedZ(1)^2];
 end
 
 %Calc Observation from noise prameter
@@ -218,7 +250,7 @@ function [z, x, u] = Observation(x, u, LM, MAX_RANGE)
         theta    = atan2(dy, dx) - x(3);
         if distance < MAX_RANGE %観測範囲内
             noise = Rsigma * randn(2, 1);
-            z = [z;[distance + noise(1) PI2PI(theta + noise(2)) LM(iz, :)]];
+            z = [z; [distance + noise(1) PI2PI(theta + noise(2)) LM(iz, :)]];
         end
     end
 end
@@ -243,7 +275,7 @@ function []=DrawGraph(result, LM, mu)
     plot(x(:,1), x(:,2),'-.b','linewidth', 4); hold on;
     plot(x(:,3), x(:,4),'r','linewidth', 4); hold on;
     plot(LM(:, 1), LM(:, 2), 'pentagram', 'MarkerSize', 15, 'MarkerFaceColor', 'g', 'MarkerEdgeColor', 'blue'); hold on;
-    for i = 1 : 4
+    for i = 1 : size(LM, 1)
         plot(mu(298, i), mu(299, i), 'pentagram', 'MarkerSize', 15); hold on;
     end
     title('PF Localization Result', 'fontsize', 16, 'fontname', 'times');
